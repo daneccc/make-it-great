369e126191bd3f450270862faab133f2577d977b
Matheus Alves <reservedwordbyme@gmail.com>
2022-11-08 09:45:42 -0300

    feat/basic-project-setup

---
+++ make-it-great/make-it-greatTests/make_it_greatTests.swift
@@ -0,0 +1,23 @@
+import XCTest
+@testable import make_it_great
+
+final class make_it_greatTests: XCTestCase {
+
+    override func setUpWithError() throws {
+
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+
+    func testExample() throws {
+
+    }
+
+    func testPerformanceExample() throws {
+        self.measure {
+        }
+    }
+
+}
