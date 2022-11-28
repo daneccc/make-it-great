3063964bf6a9194ddc123554caff4bb071134b96
Natalialocatelli <natalialocatellib@gmail.com>
2022-11-28 17:58:11 -0300

    feat onboarding

--- MakeItGreat/View/ContentView.swift
+++ MakeItGreat/View/ContentView.swift
@@ -1,15 +1,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var textFieldText: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Welcome to (AppName)")
+                .font(.title2)
+                .fontWeight(.semibold)
+        }
+        VStack {
+            Spacer()
+            Text("My name is")
+                .font(.title2)
+                .fontWeight(.semibold)
+            TextField("Please, insert your name here", text: $textFieldText)
+                .padding()
+                .background(Color.gray.opacity(0.1).cornerRadius(10))
+            Spacer()
+            Button(
+                action: {
+                    if textIsAppropriate() {
+                        saveText()
+                    }
+                }, label: {
+                    Text("Continue")
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(textIsAppropriate() ? Color.blue : Color.gray)
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                        .font(.headline)
+                })
+            
         }
         .padding()
     }
+    
+    func textIsAppropriate() -> Bool {
+        if textFieldText.count >= 3 {
+            return true
+        }
+        return false
+    }
+    
+    func saveText() {
+        textFieldText = ""
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
