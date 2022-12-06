cf4b6044f1d959f11c5a3c988d8afdb45446bf59
Natalialocatelli <natalialocatellib@gmail.com>
2022-12-06 11:18:57 -0300

    style sidebar

--- MakeItGreat/View/ContentView.swift
+++ MakeItGreat/View/ContentView.swift
@@ -6,19 +6,48 @@ struct ContentView: View {
             List {
                 NavigationLink(destination: Home()) {
                     Label("Home", systemImage: "house")
+                        .frame(height: 40)
+                        .font(.system(size: 22))
+                        .fontWeight(.regular)
                 }
+                .padding(.bottom, 10)
+                .padding(.top, 10)
+                
                 NavigationLink(destination: Profile()) {
                     Label("Profile", systemImage: "person")
+                        .frame(height: 40)
+                        .font(.system(size: 24))
+                        .fontWeight(.regular)
                 }
+                .padding(.bottom, 10)
+                .padding(.top, 10)
+                
                 NavigationLink(destination: Calendar()) {
                     Label("Calendar", systemImage: "calendar")
+                        .frame(height: 40)
+                        .font(.system(size: 24))
+                        .fontWeight(.regular)
                 }
+                .padding(.bottom, 10)
+                .padding(.top, 10)
+                
                 NavigationLink(destination: Settings()) {
                     Label("Settings", systemImage: "gearshape")
+                        .frame(height: 40)
+                        .font(.system(size: 24))
+                        .fontWeight(.regular)
                 }
+                .padding(.bottom, 10)
+                .padding(.top, 10)
+                
                 NavigationLink(destination: Mode()) {
                     Label("Mode", systemImage: "circle.lefthalf.filled")
+                        .frame(height: 40)
+                        .font(.system(size: 24))
+                        .fontWeight(.regular)
                 }
+                .padding(.bottom, 10)
+                .padding(.top, 10)
             }
             .navigationTitle("Hello User")
             Home()
